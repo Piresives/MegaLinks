1fcfd19853365d726b08da856228c1ceb7a0ce49
Ives Pires de Miranda <92396365+Piresives@users.noreply.github.com>
2022-09-22 14:14:06 -0300

    add-script.js

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      maxParticles: 200,
+      sizeVariations: 4,
+      speed: 0.3,
+      color: '#ffffff',
+      minDistance: 100,
+      connectParticles: false,
+        responsive: [
+            {
+                breakpoint:
+                    768
+                ,
+                options: {
+                    maxParticles:
+                        200
+                    ,
+                    color:
+                        '#ffffff'
+                    ,
+                    connectParticles:
+                        false
+                }
+            }, {
+                breakpoint:
+                    425
+                ,
+                options: {
+                    maxParticles:
+                        100
+                    ,
+                    connectParticles:
+                        true
+                }
+            }, {
+                breakpoint:
+                    320
+                ,
+                options: {
+                    maxParticles:
+                        0
+
+                    // disables particles.js
+                }
+            }
+        ]
+    });
+}
